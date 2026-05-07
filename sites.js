280f3f5220ac954a11bf9fb9dcdcf8a8d9a78d92
tensuio <mestouradjassem+github@gmail.com>
2026-05-07 10:06:23 +0100

    Update sites.js

--- sites.js
+++ sites.js
@@ -2,8 +2,4 @@ const SITES = [
   { name: "ChatGPT", url: "https://chatgpt.com" },
   { name: "YouTube", url: "https://youtube.com" },
   { name: "GitHub", url: "https://github.com" },
-  {
-    name: "Pixeldrain Bypass",
-    url: "https://pixeldrain-bypass.gamedrive.org/",
-  },
 ];
